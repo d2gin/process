4c5534d20c7a2727aadf40e315ea8bad42473b92
d2gin <672636920@qq.com>
2023-05-14 21:51:06 +0800

    updated

--- src/Daemon.php
+++ src/Daemon.php
@@ -2,23 +2,21 @@
 
 namespace icy8\process;
 
+use Exception;
 use icy8\EventBus\Emitter;
 
 class  Daemon
 {
-    const stdin_redirect  = 2;
-    const stdout_redirect = 4;
-    const stderr_redirect = 8;
     protected $pid;
     protected $process;
     protected $event;
-    public    $pidFile     = '';
     protected $initialized = false;
+    public    $pidFile     = '';
 
     public function __construct()
     {
         if (PHP_SAPI != 'cli') {
-            throw new \Exception('allow in cli mode');
+            throw new Exception('This command can be run only in cli mode');
         }
         $this->event = new Emitter();
     }
@@ -27,9 +25,11 @@ class  Daemon
      * 初始化
      * @return $this
      */
-    public function init()
+    protected function init()
     {
-        if ($this->initialized) return $this;
+        if ($this->initialized) {
+            return $this;
+        }
         if (empty($this->pidFile)) {
             $backtrace     = \debug_backtrace();
             $_startFile    = $backtrace[\count($backtrace) - 1]['file'];
@@ -40,25 +40,34 @@ class  Daemon
         return $this;
     }
 
+    /**
+     * 运行
+     * @throws Exception
+     */
     public function run()
     {
         $opt = array_fill_keys(array_keys(getopt("", ["start", "stop", "status", "reload"])), 1);
         $this->init();
-        if ($opt['start']) {
+        if (@$opt['start']) {
             $this->start(...func_get_args());
-        } else if ($opt['stop']) {
+        } else if (@$opt['stop']) {
             $this->stop();
-        } else if ($opt['status']) {
+        } else if (@$opt['status']) {
             $this->status();
-        } else if ($opt['reload']) {
+        } else if (@$opt['reload']) {
             $this->reload();
         }
     }
 
+    /**
+     * 开始制作
+     * @param null $process
+     * @throws Exception
+     */
     public function start($process = null)
     {
         if (is_file($this->pidFile)) {
-            throw new \Exception("process [" . file_get_contents($this->pidFile) . '] is running');
+            throw new Exception("process [" . file_get_contents($this->pidFile) . '] is running');
         } else if (!$this->process && $process) {
             $this->process = $process;
         }
@@ -66,7 +75,9 @@ class  Daemon
         $this->daemon();
         // 脚本运行结束
         register_shutdown_function(function () {
-            if ($this->pid === getmypid()) @unlink($this->pidFile);
+            if ($this->pid === getmypid() && is_file($this->pidFile)) {
+                @unlink($this->pidFile);
+            }
         });
         // 捕获退出信号
         pcntl_signal(SIGTERM, [$this, "sigHandler"]);
@@ -74,22 +85,28 @@ class  Daemon
         pcntl_async_signals(true);
         $this->event->trigger('started', $this);
         // 不强制配置业务代码
-        if($this->process) {
+        if ($this->process) {
             call_user_func_array($this->process, [$this]);
         }
     }
 
+    /**
+     * 结束运行
+     */
     public function stop()
     {
         $this->event->trigger('stoping', $this);
         $pid = @file_get_contents($this->pidFile);
         if ($pid) {
             posix_kill($pid, SIGTERM);
-            @unlink($this->pidFile);
         }
         $this->event->trigger('stoped', $this);
+        exit(0);
     }
 
+    /**
+     * @throws Exception
+     */
     public function reload()
     {
         $this->stop();
@@ -101,8 +118,12 @@ class  Daemon
         if (is_file($this->pidFile)) {
             echo "process is running" . PHP_EOL;
         } else echo "process is not running" . PHP_EOL;
+        exit(0);
     }
 
+    /**
+     * @throws Exception
+     */
     protected function daemon()
     {
         $pid = pcntl_fork();
@@ -110,7 +131,7 @@ class  Daemon
             // 退出父进程
             exit;
         } else if ($pid < 0) {
-            throw new \Exception('err');
+            throw new Exception('err');
         }
         // 子进程申请组长id
         if (posix_setsid() === -1) die;
@@ -119,11 +140,13 @@ class  Daemon
         // 第二次fork
         $pid = pcntl_fork();
         if ($pid > 0) {
-            // 继续退出子进程
-            @file_put_contents($this->pidFile, $pid);
+            if ($this->pidFile) {
+                @file_put_contents($this->pidFile, $pid);
+            }
+            // 继续退出进程
             exit;
         } else if ($pid < 0) {
-            throw new \Exception('err2');
+            throw new Exception('err2');
         }
         $this->pid = getmypid();
         // 保留孙子进程
@@ -131,11 +154,17 @@ class  Daemon
         $this->stdRedirect();
     }
 
-    protected function sigHandler($sig)
+    /**
+     * 进程信号
+     * @param $sig
+     */
+    public function sigHandler($sig)
     {
         switch ($sig) {
             case SIGTERM:
-                @unlink($this->pidFile);
+                if (is_file($this->pidFile)) {
+                    @unlink($this->pidFile);
+                }
                 exit(1);
                 break;
             default:
@@ -145,26 +174,22 @@ class  Daemon
     /**
      * 标准输出重定向
      */
-    protected function stdRedirect($toclose = self::stdin_redirect | self::stdout_redirect | self::stderr_redirect)
+    protected function stdRedirect()
     {
-        if ($toclose & self::stdin_redirect) {
-            fclose(STDIN); //Resource id #1
-            // 重新占位资源
-            global $stdin;
-            $stdin = fopen('/dev/null', 'r');
-        }
-        if ($toclose & self::stdout_redirect) {
-            fclose(STDOUT);//Resource id #2
-            // 重新占位资源
-            global $stdout;
-            $stdout = fopen('/dev/null', 'wb');
-        }
-        if ($toclose & self::stderr_redirect) {
-            fclose(STDERR);//Resource id #3
-            // 重新占位资源
-            global $stderr;
-            $stderr = fopen('/dev/null', 'wb');
-        }
+        fclose(STDIN); //Resource id #1
+        // 重新占位资源
+        global $stdin;
+        $stdin = fopen('/dev/null', 'r');
+
+        fclose(STDOUT);//Resource id #2
+        // 重新占位资源
+        global $stdout;
+        $stdout = fopen('/dev/null', 'wb');
+
+        fclose(STDERR);//Resource id #3
+        // 重新占位资源
+        global $stderr;
+        $stderr = fopen('/dev/null', 'wb');
     }
 
     public function getPid()
--- src/Worker.php
+++ src/Worker.php
@@ -3,6 +3,7 @@
 namespace icy8\process;
 //declare(ticks=1);
 
+use Exception;
 use icy8\EventBus\Emitter;
 
 class Worker
@@ -12,9 +13,8 @@ class Worker
     protected        $startFile;
     protected static $masterPid; // 主进程id
     protected        $process;   // 工作进程
-    protected        $processes  = []; // 按照业务分配进程
-    public           $title      = 'icy8-worker';// 进程名称
     protected        $workerPids = []; // 子进程pid
+    public           $title      = 'icy8-worker';// 进程名称
     public           $total      = 1;  // 需要运行的进程数 0代表无限个 进程会一直运行
     public           $max        = 0;  // 允许同时打开的进程数 0不限制
 
@@ -23,12 +23,15 @@ class Worker
         // 检查环境
         $this->isSupported();
         $this->event     = new Emitter(); // 事件发布
-        self::$masterPid = getmypid();
-        $this->pid       = getmypid();
         $backtrace       = \debug_backtrace();
         $this->startFile = $backtrace[\count($backtrace) - 1]['file'];
     }
 
+    /**
+     * 运行
+     * @param null $process
+     * @throws Exception
+     */
     public function run($process = null)
     {
         $this->init($process);
@@ -43,20 +46,19 @@ class Worker
         register_shutdown_function([$this, 'stop']);
         // 主进程保持运行
         while (!empty($this->workerPids)) {
-            $pid = pcntl_wait($status);
-            $this->whenChildWorkerFinish($pid, $status);
+            usleep(1000000);
         }
     }
 
     /**
      * 初始化
      * @param null $process
-     * @throws \Exception
+     * @throws Exception
      */
-    public function init($process = null)
+    protected function init($process = null)
     {
-        // 异步分发信号
-        pcntl_async_signals(true);
+        self::$masterPid = getmypid();
+        $this->pid       = getmypid();
         $this->bindProcess($process);
         // 注册监听终止进程信号
         pcntl_signal(SIGUSR1, [$this, "sigHandler"]);
@@ -66,16 +68,18 @@ class Worker
         pcntl_signal(SIGINT, [$this, 'sigHandler']);
         // kill信号
         pcntl_signal(SIGTERM, [$this, 'sigHandler']);
+        // 异步分发信号
+        pcntl_async_signals(true);
     }
 
     /**
      * 获取业务逻辑
      * @return mixed
      */
-    public function fetchProcess()
+    protected function fetchProcess()
     {
-        if (!empty($this->processes)) {
-            return array_shift($this->processes);
+        if (is_array($this->process)) {
+            return array_shift($this->process);
         }
         return $this->process;
     }
@@ -84,23 +88,22 @@ class Worker
      * 绑定工作进程
      * @param $process
      * @return $this
-     * @throws \Exception
+     * @throws Exception
      */
     public function bindProcess($process)
     {
+        $this->process = $process;
         if (!$process) {
-            throw new \Exception('invalid process');
+            throw new Exception('Invalid process');
         } else if (is_array($process)) {
-            $this->processes = $process;
-            $this->total     = count($process);
-        } else {
-            $this->process = $process;
+            $this->total = count($process);
         }
         return $this;
     }
 
     /**
      * 终止当前进程
+     * @throws Exception
      */
     public function stop()
     {
@@ -115,12 +118,12 @@ class Worker
     /**
      * 杀死所有子进程
      * @return bool
-     * @throws \Exception
+     * @throws Exception
      */
-    public function killAll()
+    protected function killAll()
     {
         if (!$this->isMaster()) {
-            throw new \Exception('only run in master worker');
+            throw new Exception('Only run in master worker');
         }
         foreach ($this->workerPids as $pid) {
             if ($pid === $this->getMasterPid()) {
@@ -128,19 +131,23 @@ class Worker
                 continue;
             }
             posix_kill($pid, SIGUSR1);
+            pcntl_waitpid($pid, $status);
+            $this->whenChildWorkerFinish($pid, $status);
         }
         return true;
     }
 
     /**
      * 新建一个子进程
+     * @param null $process
+     * @throws Exception
      */
-    public function fork()
+    public function fork($process = null)
     {
         $this->event->trigger('worker_forking', $this);
-        $process = $this->fetchProcess();
+        !$process && $process = $this->fetchProcess();
         if (!$process) {
-            throw new \Exception('have no process to run');
+            throw new Exception('Have no process to run');
         }
         $pid = pcntl_fork();
         if ($pid === 0) {
@@ -163,7 +170,7 @@ class Worker
     /**
      * 如果超出最大进程限制，则等待子进程退出
      */
-    public function waitingWorkerReleaseIfExceed()
+    protected function waitingWorkerReleaseIfExceed()
     {
         if ($this->max > 0 && count($this->workerPids) >= $this->max) {
             $pid = pcntl_wait($status);
@@ -176,7 +183,7 @@ class Worker
      * @param $pid
      * @param $status
      */
-    public function whenChildWorkerFinish($pid, $status)
+    protected function whenChildWorkerFinish($pid, $status)
     {
         if (isset($this->workerPids[$pid])) {
             unset($this->workerPids[$pid]);
@@ -187,18 +194,15 @@ class Worker
     /**
      * 捕获进程信号
      * @param $signo
+     * @throws Exception
      */
     public function sigHandler($signo)
     {
         switch ($signo) {
-            case SIGUSR1:
-                $this->stop();
-                break;
             case SIGCHLD:
                 break;
+            case SIGUSR1:
             case SIGINT:
-                $this->stop();
-                break;
             case SIGTERM:
                 $this->stop();
                 break;
@@ -208,23 +212,23 @@ class Worker
 
     /**
      * 检查环境
-     * @throws \Exception
+     * @throws Exception
      */
     public function isSupported()
     {
         if (PHP_SAPI != 'cli') {
-            throw new \Exception('allow in cli mode');
+            throw new Exception('allow in cli mode');
         }
         $extensions = ['pcntl', 'posix'];
         $functions  = ['pcntl_signal', 'pcntl_fork', 'getmypid', 'posix_kill'];
         foreach ($extensions as $extension) {
             if (!extension_loaded($extension)) {
-                throw new \Exception("need [{$extension}] extension");
+                throw new Exception("need [{$extension}] extension");
             }
         }
         foreach ($functions as $function) {
             if (!function_exists($function)) {
-                throw new \Exception("function [{$function}] not exists");
+                throw new Exception("function [{$function}] not exists");
             }
         }
     }
@@ -249,7 +253,7 @@ class Worker
      * 是否为master进程
      * @return bool
      */
-    public function isMaster()
+    protected function isMaster()
     {
         return getmypid() === self::$masterPid;
     }
